f5af6c9ce7ba1167902b37df87beace757a97bd6
rsnakamura <rsnakamura@acm.org>
2016-02-15 16:41:40 -0800

    Removed the django REST repository

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{pweav:0,fork:0,account:0,github:0,old:0,text:0,site:0,rest:0,django:0,util:0,framework:0,restructur:0,aggreg:0,thi:0,iperflex:0,http:[],document:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["rsnakamura"],objects:{},titleterms:{rsnakamura:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{account:0,aggreg:0,github:0,iperflex:0,old:0,page:0,pweav:0,site:0,thi:0,util:0},titles:["rsnakamura"],titleterms:{rsnakamura:0}})
